f44ccee7e0302845ec520af7b8be53f5acdc430e
hemangtrivedi <55792204+hemangtrivedi@users.noreply.github.com>
2021-11-19 20:06:09 +0530

    Update clearsave_v2.sql

--- clearsave_v2.sql
+++ clearsave_v2.sql
@@ -1,2 +1,4 @@
+delete from myTable where id between 144 and 154
+
 INSERT INTO `myTable` (`name`,`phone`,`numberrange`) VALUES   ("hemang","(224) 518-7337",7),   ("trivedi","(845) 572-2778",9),   ("Kitra
 Davidson","1-370-137-6297",4),   ("Thomas Arnold","(846) 929-3749",8),   ("Bianca Holder","1-573-817-2667",6),   ("Wing Hurley","1-707-876-4374",6),   ("Hollee Snow","(617) 558-6175",2),   ("Wallace Watts","1-848-784-4872",9),   ("April Lancaster","1-728-486-8964",7),   ("Lareina Barrett","1-877-178-1218",1);
