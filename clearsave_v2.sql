29b61047e642a7258361fd7e2e4cb6449ea4cb9b
hemangtrivedi <55792204+hemangtrivedi@users.noreply.github.com>
2021-11-19 20:03:12 +0530

    Update clearsave_v2.sql

--- clearsave_v2.sql
+++ clearsave_v2.sql
@@ -1,1 +1,2 @@
-INSERT INTO `myTable` (`name`,`phone`,`numberrange`) VALUES ('hemang','123456','12345678');
+INSERT INTO `myTable` (`name`,`phone`,`numberrange`) VALUES   ("Kyra Hanson","(224) 518-7337",7),   ("Stephanie Mcneil","(845) 572-2778",9),   ("Kitra
+Davidson","1-370-137-6297",4),   ("Thomas Arnold","(846) 929-3749",8),   ("Bianca Holder","1-573-817-2667",6),   ("Wing Hurley","1-707-876-4374",6),   ("Hollee Snow","(617) 558-6175",2),   ("Wallace Watts","1-848-784-4872",9),   ("April Lancaster","1-728-486-8964",7),   ("Lareina Barrett","1-877-178-1218",1);
